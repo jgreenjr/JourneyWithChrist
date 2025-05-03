89f191865589a99c6b2886c83d670d23163a3ec1
Gerald J Green Jr <jgreenjr@geralds-air.lan>
2025-05-03 15:57:16 -0700

    Initial commit

---
+++ test/journey_with_christ.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as JourneyWithChrist from '../lib/journey_with_christ-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/journey_with_christ-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new JourneyWithChrist.JourneyWithChristStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
